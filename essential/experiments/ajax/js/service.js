8e40f852fbc1a6d49e476071384a199485f694d3
meera1 <udani.m@husky.neu.edu>
2015-04-12 16:29:31 -0400

    Some experiments about the extra things I wanted to learn about

---
+++ essential/experiments/ajax/js/service.js
@@ -0,0 +1,42 @@
+﻿app.factory('MovieService', function MovieService($http) {
+
+    var favouriteMovies = [];
+    var searchMovies = function (searchByTitle, callback) {
+
+        var title = searchByTitle;
+        $http.jsonp("http://www.myapifilms.com/imdb?title= " + title + "&format=JSONP&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=N&exactFilter=0&limit=10&lang=en-us&actors=N&biography=0&trailer=0&uniqueName=0&filmography=0&bornDied=0&starSign=0&actorActress=0&actorTrivia=0&movieTrivia=0&awards=0&moviePhotos=N&movieVideos=N&callback=JSON_CALLBACK")
+        .success(callback);
+    }
+
+    var addToFavourites = function (movie) {
+        favouriteMovies.push(movie);
+    }
+
+    var getFavourites = function()
+    {
+        return favouriteMovies;
+    }
+
+
+  /*  var getTitle = function(id)
+    {
+        $http.jsonp("http://www.myapifilms.com/imdb?format=JSONP&idIMDB=" + id + "&callback=JSON_CALLBACK")
+            .success(function (movie) {
+                alert("hi")
+                //return movie
+            });
+ */      
+    
+
+
+
+  
+    return {  // don't created { on a new line if its on new line it means return immediately, else it means first create this obj then return
+        search: searchMovies,
+        addToFavourites: addToFavourites,
+        getFavourites: getFavourites
+       // getTitle : getTitle
+    }
+    
+});
+
---
+++ essential/experiments/svg/js/svg11.js
@@ -0,0 +1,51 @@
+﻿$(function () {
+    var param = {
+        apikey: "v6cqqs9x7f6qkucu27wnsbgn"
+    };
+    $.ajax({
+        url: "http://api.rottentomatoes.com/api/public/v1.0/movies.json?",
+        data: param,
+        dataType: "jsonp",
+        success: function (response) {
+            var movieArray = [];
+            for (var i = 0; i < response.movies.length; i++) {
+                var movie = response.movies[i];
+                movieArray.push(movie.title);
+            }
+
+        }
+    });
+
+    $(".details").click(display);
+});
+function display() {
+    var text = $(".titleofmovie").val();
+    var param = {
+        apikey: "v6cqqs9x7f6qkucu27wnsbgn",
+        q: text,
+        page_limit: "10"
+    };
+    $.ajax({
+        url: "http://api.rottentomatoes.com/api/public/v1.0/movies.json?",
+        data: param,
+        dataType: "jsonp",
+        success: function (response) {
+            var ul = $(".tableforrecord");
+            ul.empty();
+
+            for (var i = 0; i < response.movies.length; i++) {
+                var movie = response.movies[i];
+                var name = movie.title;
+                var year = movie.year;
+                var duration = movie.runtime;
+                var rating = movie.critics_consensus;
+                var instance = $(".listofmovies .Movies:first-child").clone();
+                instance.find(".title").html(name);
+                instance.find(".year").html(year);
+                instance.find(".duration").html(duration);
+                instance.filter(".rating").html(rating);
+                ul.append(instance);
+            }
+        }
+    });
+}
